498588571e4259de0a410877601c53be40a4320d
Asher Glick <asher.glick@gmail.com>
2021-07-05 03:51:55 -0500

    Burrito can now load basic taco maps that have been converted to json with the taco_to_burrito.py script

--- Spatial.gd
+++ Spatial.gd
@@ -3,21 +3,32 @@ extends Spatial
 var server := UDPServer.new()
 var peers = []
 
+var map_id:int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().get_root().set_transparent_background(true)
 	#ProjectSettings.set_setting("display/window/size/always_on_top", true)
 	#OS.set_window_always_on_top(true)
-	
+
 	OS.window_maximized = false
 	OS.window_size = Vector2(1920, 1080)
-	
-	var clickthrough: PoolVector2Array = [Vector2(100,100),Vector2(100,200),Vector2(200,200),Vector2(200,100)]
-	
-	OS.set_window_mouse_passthrough(clickthrough)
-	
+	set_minimal_mouse_block()
 	server.listen(4242)
 
+	# load_taco_markers("/home/vault/Downloads/tw_ALL_IN_ONE/tw_mc_coretyria.xml")
+
+func set_minimal_mouse_block():
+	var clickthrough: PoolVector2Array = [
+		Vector2(287,0),
+		Vector2(314,0),
+		Vector2(314,32),
+		Vector2(287,32)
+	]
+	OS.set_window_mouse_passthrough(clickthrough)
+
+func set_maximal_mouse_block():
+	OS.set_window_mouse_passthrough([])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -85,8 +96,21 @@ func decode_frame_packet(spb: StreamPeerBuffer):
 	var compass_rotation_is_enabled: bool = (ui_flags & 0x04) == 0x04;
 	var game_is_focused: bool = (ui_flags & 0x08) == 0x08;
 	var in_competitive_game_mode: bool = (ui_flags & 0x10) == 0x10;
-	var textbox_has_focus: bool = (ui_flags & 0x20) == 0x20;
+	var chatbox_has_focus: bool = (ui_flags & 0x20) == 0x20;
 	var in_combat: bool = (ui_flags & 0x40) == 0x40;
+	var unchecked_flag = (ui_flags & 0xFFFFFF80) != 0x00000000;
+
+	$PathsAndIcons.visible = !map_is_open
+
+	if unchecked_flag:
+		print("Unchecked Flag", (ui_flags & 0xFFFFFF80))
+	
+#	if game_is_focused:
+#		print("game_is_focused")
+#	else:
+#		print("game is not focused")
+
+#	print("chatbox has focus ", chatbox_has_focus)
 	
 	# Position the camera in the same location as it in in-game
 	$CameraMount.translation.x = camera_position.x
@@ -97,6 +121,8 @@ func decode_frame_packet(spb: StreamPeerBuffer):
 	$CameraMount/Camera.rotation.x = asin(camera_facing.y)
 	$CameraMount.rotation.y = -atan2(camera_facing.x, camera_facing.z)
 	#print(-atan2(camera_facing.x, camera_facing.z))
+	
+	#print(player_position.y)
 
 #	$MeshInstance.translation.x = player_position.x
 #	$MeshInstance.translation.y = player_position.y
@@ -106,7 +132,7 @@ func decode_frame_packet(spb: StreamPeerBuffer):
 func decode_context_packet(spb: StreamPeerBuffer):
 	var compas_width: int = spb.get_16()
 	var compass_height: int = spb.get_16()
-	var map_id: int = spb.get_32()
+	self.map_id = spb.get_32()
 	
 	var identity_length: int = spb.get_32()
 	var identity_str = spb.get_utf8_string(identity_length)
@@ -118,3 +144,87 @@ func decode_context_packet(spb: StreamPeerBuffer):
 	# According to https://thatshaman.com/tools/fov/ the FOV range is (25 -> 70)
 	# This function maps a value between 0.436 and 1.222 to a value between 25 and 70
 	$CameraMount/Camera.fov = (((identity["fov"] - 0.436) / 0.786) * 45) + 25
+
+var markerdata = {}
+var marker_file_path = ""
+func load_taco_markers(marker_json_file):
+	self.marker_file_path = marker_json_file
+	print("Loading Path", marker_json_file)
+	
+	var file = File.new()
+	file.open(marker_json_file, file.READ)
+	var text = file.get_as_text()
+	self.markerdata = JSON.parse(text).result
+	gen_map_markers()
+
+var path_scene = load("res://Path.tscn")
+var icon_scene = load("res://Icon.tscn")
+
+
+func gen_map_markers():
+	var paths_and_icons = $PathsAndIcons
+
+	if str(map_id) in markerdata:
+		print("Found Map")
+		var map_markerdata = markerdata[str(map_id)]
+		print("Paths Found:", len(map_markerdata["paths"]))
+		for path in map_markerdata["paths"]:
+			#print("Path", path["texture"])
+			var new_path = path_scene.instance()
+			var new_curve = Curve3D.new()
+			for point in path["points"]:
+				new_curve.add_point(Vector3(point[0], point[1], -point[2]))
+			print(new_curve.get_point_count())
+			new_path.curve = new_curve
+
+			paths_and_icons.add_child(new_path)
+			
+			var texture_file = File.new()
+			var image = Image.new()
+			texture_file.open(self.marker_file_path.get_base_dir() + "/"+ path["texture"], File.READ)
+			image.load_png_from_buffer(texture_file.get_buffer(texture_file.get_len()))
+			texture_file.close()
+			image.lock()
+			
+			var texture = ImageTexture.new()
+			texture.create_from_image(image, 6)
+#			var texture = Image.new()
+#			texture.load(self.marker_file_path.get_base_dir() + "/"+ path["texture"])
+#			print(texture)
+			new_path.get_node("CSGPolygon").material.set_shader_param("texture_albedo", texture)
+#
+			# Create a new path instance
+		for icon in map_markerdata["icons"]:
+			var new_icon = icon_scene.instance()
+			new_icon.translation = Vector3(icon["position"][0], icon["position"][1], -icon["position"][2])
+			
+			var texture_file = File.new()
+			var image = Image.new()
+			texture_file.open(self.marker_file_path.get_base_dir() + "/"+ icon["texture"], File.READ)
+			image.load_png_from_buffer(texture_file.get_buffer(texture_file.get_len()))
+			texture_file.close()
+			image.lock()
+			
+			var texture = ImageTexture.new()
+			texture.create_from_image(image) #, 6)
+			new_icon.texture = texture
+			
+			paths_and_icons.add_child(new_icon)
+			#new_icon.translation = Vector3()
+			#print("Icon", icon["texture"])
+		#	pass
+
+func _on_main_menu_toggle_pressed():
+	$Control/FileDialog.show()
+	set_maximal_mouse_block()
+	pass # Replace with function body.
+
+
+func _on_FileDialog_hide():
+	set_minimal_mouse_block()
+	pass # Replace with function body.
+
+
+func _on_FileDialog_file_selected(path):
+	load_taco_markers(path)
+	pass # Replace with function body.
---
+++ taco_to_burrito.py
@@ -0,0 +1,313 @@
+import os
+
+import xml.etree.ElementTree as ET
+import struct
+
+from typing import *
+import json
+
+output_dir = ""
+
+def main():
+    source_path: str = "tekkit_workshop_taco_markers"
+
+    for file in os.scandir(source_path):
+        if(file.name.endswith(".xml")):
+            full_path: str = os.path.join(source_path, file.name)
+            convert_markers(full_path, "tekkit_workshop_burrito_markers")
+
+################################################################################
+# Quick error printing logic that only prints the title of a section if there
+# are any errors in it. Could be handled better by returning errors from the
+# function but that is more work to implement then this is.
+################################################################################
+eprint_title: str = ""
+eprint_trigger: bool = False
+def set_eprint_title(title: str):
+    global eprint_title
+    global eprint_trigger
+    eprint_title = title
+    eprint_trigger = False
+def eprint(*args, **kwargs):
+    global eprint_trigger
+    if not eprint_trigger:
+        eprint_trigger = True
+        print(eprint_title)
+    print(*args, **kwargs)
+
+
+################################################################################
+#
+################################################################################
+def parse_marker_category(marker_category_node, base=""):
+    required_keys = set(["DisplayName", "name" ])
+    allowed_keys = set([
+        "iconFile",
+        "iconSize", # Ignored
+        "heightOffset",
+
+        "minSize", # Ignored
+        "mapDisplaySize", # Ignored
+        "fadeNear", # Ignored
+        "fadeFar",  # Ignored
+        "inGameVisibility", # Ignored
+        "miniMapVisibility", # Ignored
+        "mapVisibility",     # Ignored
+        "IsSeparator", # Ignored - Only useful for menu system not being implemented
+        "behavior", # Ignored
+        "toggleCategory", # Ignored - Used for unimplemented functionality
+        "resetLength", # Ignored
+    ])
+
+
+    metadata_tree = {}
+
+    if marker_category_node.tag != "MarkerCategory":
+        eprint("MarkerCategory Child not  MarkerCategory", marker_category_node.tag)
+
+    attribs = marker_category_node.attrib
+    for present_key in attribs:
+        if present_key not in required_keys and present_key not in allowed_keys:
+            eprint("key {} found that is not allowed".format(present_key))
+
+    for required_key in required_keys:
+        if required_key not in attribs:
+            eprint("key {} missing from marker category".format(required_key))
+
+
+    limited_attribs = {}
+    if "iconFile" in attribs:
+        limited_attribs["icon_file"] = attribs["iconFile"]
+
+    if "iconSize" in attribs:
+        limited_attribs["icon_size"] = attribs["iconSize"]
+
+    if "heightOffset" in attribs:
+        limited_attribs["height_offset"] = attribs["heightOffset"]
+    else:
+        limited_attribs["height_offset"] = 0
+
+
+    name = attribs["name"]
+
+    metadata_tree[base + name] = limited_attribs
+
+    for child in marker_category_node:
+        subtree = parse_marker_category(child, base + name + ".")
+        for elem in subtree:
+            metadata_tree[elem] = subtree[elem]
+
+    return metadata_tree
+
+################################################################################
+# 
+################################################################################
+def convert_markers(xml_path: str, output_dir: str):
+    set_eprint_title(xml_path)
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+
+    if root.tag != "OverlayData":
+        eprint("Root not OverlayData", root.tag)
+
+    if root.attrib != {}:
+        eprint("Root has unknown attribs", root.attrib)
+
+    if len(root) != 2:
+        eprint("Root has {} children instead of the expected 2".format(len(root)))
+
+    if root[0].tag != "MarkerCategory":
+        eprint("First element of root is {} not MarkerCategory".format(root[0].tag))
+    else:
+        marker_metadata = parse_marker_category(root[0])
+
+    # print(marker_metadata)
+
+    if root[1].tag != "POIs":
+        eprint("First element of root is {} not POIs".format(root[1].tag))
+    else:
+        burrito_marker_data = parse_icons_and_paths(root[1], marker_metadata, os.path.dirname(xml_path))
+
+
+    with open(os.path.join(output_dir, os.path.basename(xml_path)[:-4] + ".json"), 'w') as f:
+        json.dump(burrito_marker_data, f)
+
+    # return burrito_marker_data
+    # print(marker_metadata)
+
+
+
+
+# class icon():
+
+
+# class path():
+
+# @dataclass
+# class mapdata():
+#     icons: List[icon]
+#     paths: List[path]
+
+def aprox_zero(a:float):
+    return abs(a) <= 1e-09
+
+
+def parse_icons_and_paths(poi_node, marker_metadata, dirname=""):
+
+    required_poi_attrib = set([
+        "GUID", # Ignored
+        "xpos",
+        "ypos",
+        "zpos",
+        "MapID",
+        "type",
+    ])
+    allowed_poi_attrib = set([
+        "fadeFar", # Ignored
+        "minSize", # Ignored
+        "iconSize", # Ignored
+        "heightOffset",
+        "iconFile", 
+        "mapDisplaySize", # Ignored
+        "fadeNear", # Ignored
+        "behavior", # Ignored
+        "resetLength" # Ignored
+    ])
+
+    required_trail_attrib = set([
+        "texture",
+        "animSpeed", # Ignored
+        "trailData",
+        "fadeFar", # Ignored
+        "GUID", # Ignored
+        "type", #Ignored TODO: maybe should not ignore?
+    ])
+    allowed_trail_attrib = set([
+        "mapDisplaySize",  # Ignored
+        "mapVisibility",  # Ignored
+        "miniMapVisibility",  # Ignored
+        "fadeNear",  # Ignored
+    ])
+
+    burrito_marker_data = {}
+
+
+    for child in poi_node:
+        if len(child) > 0:
+            eprint("POI element has children")
+
+        if child.tag == "POI":
+            attribs = child.attrib
+            for present_key in attribs:
+                if present_key not in required_poi_attrib and present_key not in allowed_poi_attrib:
+                    eprint("key {} found that is not allowed".format(present_key))
+
+            for required_key in required_poi_attrib:
+                if required_key not in attribs:
+                    eprint("key {} missing from marker category".format(required_key))
+
+            map_id = int(attribs["MapID"])
+            
+            if map_id not in burrito_marker_data:
+                burrito_marker_data[map_id] = {"icons":[], "paths":[]}
+
+            position: List[float] = [float(attribs["xpos"]), float(attribs["ypos"]), float(attribs["zpos"])]
+            icon_path = ""
+
+            if attribs["type"] in marker_metadata:
+                position[1] += float(marker_metadata[attribs["type"]]["height_offset"])
+                if "icon_file" in marker_metadata[attribs["type"]]:
+                    icon_path = marker_metadata[attribs["type"]]["icon_file"]
+                if "iconFile" in attribs:
+                    icon_path = attribs["iconFile"]
+            else:
+                eprint("Type Not Found", attribs["type"])
+
+            if icon_path == "":
+                eprint("No Icon Found")
+                pass
+            else:
+                # print("icon found")
+                pass
+
+
+            burrito_marker_data[map_id]["icons"].append({
+                "position": position,
+                "texture": copyimage(icon_path)
+            })
+
+        elif child.tag == "Trail":
+            attribs = child.attrib
+            for present_key in attribs:
+                if present_key not in required_trail_attrib and present_key not in allowed_trail_attrib:
+                    eprint("key {} found that is not allowed".format(present_key))
+
+            for required_key in required_trail_attrib:
+                if required_key not in attribs:
+                    eprint("key {} missing from marker category".format(required_key))
+
+            map_id, points = open_trail_format(os.path.join(dirname, attribs["trailData"]))
+
+
+            point_subsections = [[]]
+            for point in points:
+                if aprox_zero(point[0]) and aprox_zero(point[1]) and aprox_zero(point[2]):
+                    point_subsections.append([])
+                else:
+                    point_subsections[-1].append(point)
+
+            for point_subsection in point_subsections:
+
+                if map_id not in burrito_marker_data:
+                    burrito_marker_data[map_id] = {"icons":[], "paths":[]}
+
+                if attribs["type"] in marker_metadata:
+                    for point in point_subsection:
+                        old_point = point[1]
+
+                        point[1] += marker_metadata[attribs["type"]]["height_offset"]
+
+                        # print(old_point, marker_metadata[attribs["type"]]["height_offset"], point[1])
+                else:
+                    eprint("Type Not Found", attribs["type"])
+
+
+                burrito_marker_data[map_id]["paths"].append(
+                    {
+                        "texture": copyimage(attribs["texture"]),
+                        "points": point_subsection
+                    }
+                )
+
+
+        else:
+            eprint("First element of root is {} not POIs".format(root[1].tag))
+
+    return burrito_marker_data
+
+
+
+def copyimage(image_path):
+    return image_path
+
+
+def open_trail_format(trl_path: str) -> Tuple[int, List[float]]:
+    with open(trl_path, 'rb') as f:
+        file_bytes = f.read()
+
+    version = struct.unpack("<i", memoryview(file_bytes)[0:4])[0] # Ignored
+    map_id = struct.unpack("<i", memoryview(file_bytes)[4:8])[0]
+
+    index = 8
+
+    points = []
+    while index < len(file_bytes):
+        pointx, pointy, pointz = struct.unpack("<fff", memoryview(file_bytes)[index:index+12])[0:3]
+        index += 12
+        points.append([pointx, pointy, pointz])
+
+    return map_id, points
+
+
+if __name__ == "__main__":
+    main()
