853db76fc785c2f6abbc7a45703f5aca938f0f38
Jia Long Loh <jlloh89@gmail.com>
2021-10-25 23:57:46 +0800

    Further reduce size

--- Icon2D.gd
+++ Icon2D.gd
@@ -5,6 +5,6 @@ const Utils = preload("utils.gd")
 func set_icon_image(texture_path: String):
 	var texture = Utils.new().generate_texture(texture_path, 'RGB')
 	self.texture = texture
-	# TODO: Figure out a better heuristic. Why 0.1?
-	var scaling_factor = 0.1
+	# TODO: Figure out a better heuristic. Why 0.05?
+	var scaling_factor = 0.05
 	self.apply_scale(Vector2(scaling_factor, scaling_factor))
