e39950bd875bc3d38a20ed43cbe6f29de4d3f565
klingbolt <klingbeilrichard@gmail.com>
2022-08-21 18:27:02 -0400

    Removed erronious include guard

--- xml_converter/src/string_helper.hpp
+++ xml_converter/src/string_helper.hpp
@@ -14,7 +14,5 @@ vector<string> split(string input, string delimiter);
 
 string normalize_type_name(string type_name);
 
-#define _BASE64_H_
-
 std::string base64_encode(uint8_t const* buf, unsigned int bufLen);
 std::vector<uint8_t> base64_decode(std::string const&);
