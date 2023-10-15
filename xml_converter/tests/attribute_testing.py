9e21d431ca289fdb90039d500c042e288d42d6ce
klingbolt <klingbeilrichard@gmail.com>
2023-10-15 19:18:57 -0400

    Moved test case values to seperate JSON

--- xml_converter/tests/attribute_testing.py
+++ xml_converter/tests/attribute_testing.py
@@ -1,24 +1,16 @@
-from dataclasses import dataclass
 import subprocess
+import json
 import re
 import os
-from typing import Dict, List
+from typing import List
 
 # Path to compiled C++ executable
 xml_converter: str = "../build/xml_converter"
 
-
-@dataclass
-class Attribute_Tests:
-    attribute_name: str
-    attribute_path: str
-    tests: List[str]
-    output_result: Dict[str, List[str]]
-    expect_output_to_be_equal: Dict[str, bool]
-    expect_files_to_be_equal: Dict[str, bool]
+json_file_path: str = "./test_cases.json"
 
 
-def run_xml_converter(*args):
+def run_xml_converter(*args: str) -> str:
     try:
         # Build the command to execute the C++ program with the desired function and arguments
         cmd = [xml_converter] + list(args)
@@ -33,10 +25,10 @@ def run_xml_converter(*args):
             return result.stderr
     except Exception as e:
         print("Error:", e)
-        return None
+        return ""
 
 
-def are_files_equal(file_path1, file_path2) -> bool:
+def are_files_equal(file_path1: str, file_path2: str) -> bool:
     try:
         with open(file_path1, 'rb') as file1, open(file_path2, 'rb') as file2:
             content1 = file1.read()
@@ -49,7 +41,7 @@ def are_files_equal(file_path1, file_path2) -> bool:
         return False
 
 
-def are_arrays_equal(array1, array2) -> bool:
+def are_arrays_equal(array1: List[str], array2: List[str]) -> bool:
     if len(array1) != len(array2):
         return False
 
@@ -71,70 +63,66 @@ chrono_patterns = [
 
 
 if __name__ == "__main__":
-    # TODO: This will need to be extracted as more attributes and tests are added
-    attribute: Attribute_Tests = Attribute_Tests(
-        attribute_name="can_fade",
-        attribute_path="../test_cases/can_fade_tests",
-        tests=["_is_correct", "_is_different_file", "_is_wrong_output"],
-        output_result={
-            "_is_correct": ['Loading taco pack ../test_cases/can_fade_tests/can_fade_is_correct.xml'],
-            "_is_different_file": ['Loading taco pack ../test_cases/can_fade_tests/can_fade_is_different_file.xml'],
-            "_is_wrong_output": ['WRONG']
-        },
-        expect_output_to_be_equal={
-            "_is_correct": True,
-            "_is_different_file": True,
-            "_is_wrong_output": False,
-        },
-        expect_files_to_be_equal={
-            "_is_correct": True,
-            "_is_different_file": False,
-            "_is_wrong_output": True,
-        }
-    )
+    try:
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
 
-    # Ensure that the test output directory is created
-    if not os.path.exists(attribute.attribute_path + "-output/"):
-        try:
-            os.makedirs(attribute.attribute_path + "-output/")
-        except OSError as e:
-            print(f"Error: {e}")
+        for attribute_name in data.keys():
+            print(attribute_name)
+            attribute_data = data[attribute_name]
 
-    for test in attribute.tests:
-        arg1 = attribute.attribute_path + "/" + attribute.attribute_name + test + ".xml"
-        arg2 = attribute.attribute_path + "-output/" + attribute.attribute_name + test + ".xml"
+            # Ensure that the test output directory is created
+            if not os.path.exists(attribute_data["path"] + "-output/"):
+                try:
+                    os.makedirs(attribute_data["path"] + "-output/")
+                except OSError as e:
+                    print(f"Error: {e}")
 
-        result = run_xml_converter(arg_input_xml, arg1, arg_output_xml, arg2)
-        if result is not None:
-            # Remove lines about how long it took to run
-            split_result = result.split("\n")
-            filtered_array = []
-            for line in split_result:
-                match_found = False
-                for pattern in chrono_patterns:
-                    if re.search(pattern, line) or line == "":
-                        match_found = True
-                        break
-                if not match_found:
-                    filtered_array.append(line)
+            for test in attribute_data["tests"].keys():
+                arg1 = attribute_data["path"] + "/" + attribute_name + test + ".xml"
+                arg2 = attribute_data["path"] + "-output/" + attribute_name + test + ".xml"
 
-            output_test: bool = True
-            file_test: bool = True
+                result = run_xml_converter(arg_input_xml, arg1, arg_output_xml, arg2)
+                if result != "":
+                    # Remove lines that state how long it took to run
+                    split_result = result.split("\n")
+                    filtered_array: List[str] = []
+                    for line in split_result:
+                        match_found = False
+                        if line == "":
+                            continue
+                        for pattern in chrono_patterns:
+                            if re.search(pattern, line):
+                                match_found = True
+                                break
+                        if not match_found:
+                            filtered_array.append(line)
 
-            if are_arrays_equal(filtered_array, attribute.output_result[test]) != attribute.expect_output_to_be_equal[test]:
-                print(f"Output did not match for test {attribute.attribute_name}{test}")
-                print(f"Expected {attribute.output_result[test]} and got {filtered_array}")
-                output_test = False
+                    output_test: bool = True
+                    file_test: bool = True
+                    expected_results = attribute_data["tests"][test]
 
-            if are_files_equal(arg1, arg2) != attribute.expect_files_to_be_equal[test]:
-                print(f"Files were incorrect for test {attribute.attribute_name}{test}")
-                print(f"Expected {attribute.expect_files_to_be_equal[test]}")
-                print(f"arg1: {arg1}")
-                print(f"arg2: {arg2}")
-                file_test = False
+                    if are_arrays_equal(filtered_array, expected_results["output_result"]) != expected_results["expect_output_to_be_equal"]:
+                        print(f"Output did not match for test {attribute_name}{test}")
+                        print(f"Expected {expected_results['output_result']} and got {filtered_array}")
+                        output_test = False
 
-            if output_test and file_test:
-                print(f"Successful test {attribute.attribute_name}{test}")
+                    if are_files_equal(arg1, arg2) != expected_results["expect_files_to_be_equal"]:
+                        print(f"Files were incorrect for test {attribute_name}{test}")
+                        print(f"Expected {expected_results['expect_files_to_be_equal']}")
+                        print(f"arg1: {arg1}")
+                        print(f"arg2: {arg2}")
+                        file_test = False
 
-        else:
-            print(f"Failed to execute {attribute.attribute_name}{test}")
+                    if output_test and file_test:
+                        print(f"Success: test {attribute_name}{test}")
+
+                else:
+                    print(f"Failed to execute {attribute_name}{test}")
+
+    except FileNotFoundError:
+        print(f"The file '{json_file_path}' does not exist.")
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
