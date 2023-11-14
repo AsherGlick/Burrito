297acf72797abec983daf79f510920e5858630a9
klingbolt <klingbeilrichard@gmail.com>
2023-11-13 23:34:49 -0500

    Added tests for profession filter

--- xml_converter/intigration_tests/testcases.py
+++ xml_converter/intigration_tests/testcases.py
@@ -90,4 +90,10 @@ testcases: List[Testcase] = [
         expected_output_xml_path="./expected_outputs/xml_festival_filter",
         expected_output_proto_path="./expected_outputs/proto_festival_filter",
     ),
+    Testcase(
+        name="profession_filter",
+        xml_input_paths=["./inputs/xml_profession_filter"],
+        expected_output_xml_path="./expected_outputs/xml_profession_filter",
+        expected_output_proto_path="./expected_outputs/proto_profession_filter",
+    ),
 ]
