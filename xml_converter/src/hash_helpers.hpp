fe1bf76be98ef904d63b83a471f3b9eb565318fe
Asher Glick <asher.glick@gmail.com>
2024-01-21 22:11:18 -0600

    adding helper functions to generate simple hashed values

---
+++ xml_converter/src/hash_helpers.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+class Hash64 {
+ private:
+    uint64_t hash;
+
+ public:
+    Hash64();
+    explicit Hash64(uint64_t init);
+    void update(const unsigned char* str, size_t length);
+    void update(const std::string& str);
+    std::string hex() const;
+};
+
+class Hash128 {
+ private:
+    uint64_t upper;
+    uint64_t lower;
+
+ public:
+    Hash128();
+    Hash128(uint64_t upper, uint64_t lower);
+    void update(const unsigned char* str, size_t length);
+    void update(const std::string& str);
+    std::string hex() const;
+    std::string base64() const;
+};
