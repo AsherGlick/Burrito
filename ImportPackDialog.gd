7757841adc792e923375617613f75d450c406017
klingbolt <klingbeilrichard@gmail.com>
2024-04-27 15:35:15 -0400

    Move variable creation to ready function

--- ImportPackDialog.gd
+++ ImportPackDialog.gd
@@ -1,20 +1,21 @@
 extends FileDialog
+
 const FileHandler = preload("res://FileHandler.gd")
 var downloaded_markers_dir: String
 var unsaved_markers_dir: String
 var user_data_dir: String
 
-func _on_FileDialog_dir_selected(dir_path):
-	print("Selected folder:", dir_path)
+func _ready():
 	self.user_data_dir = str(OS.get_user_data_dir())
 	self.downloaded_markers_dir = self.user_data_dir.plus_file("packs")
 	self.unsaved_markers_dir = self.user_data_dir.plus_file("protobins")
 	FileHandler.create_directory_if_missing(self.downloaded_markers_dir)
 	FileHandler.create_directory_if_missing(self.unsaved_markers_dir)
+
+func _on_FileDialog_dir_selected(dir_path):
+	print("Selected folder:", dir_path)
 	var new_path: String = self.downloaded_markers_dir.plus_file(dir_path.get_file())
 	FileHandler.create_directory_if_missing(new_path)
-	#else: 
-	#	#Pop up here to confirm overwrite?
 	var args: PoolStringArray = [
 		"--input-taco-path", dir_path,
 		"--output-waypoint-path", new_path,
