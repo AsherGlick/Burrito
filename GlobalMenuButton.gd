5a3bec167eceab2d01b8fa87ae2caf79956f4307
aukuste <admin@auku.st>
2025-04-17 15:11:26 +0200

    Remove debug print in GlobalMenuButton.gd

--- GlobalMenuButton.gd
+++ GlobalMenuButton.gd
@@ -6,6 +6,5 @@ func _ready():
 
 func _update_global_menu_button_position():
 	if Settings.override_burrito_icon_position_enabled == true:
-		print(Settings.override_burrito_icon_horizontal_position, " ", Settings.override_burrito_icon_vertical_position)
 		self.set_position(Vector2(Settings.override_burrito_icon_horizontal_position, Settings.override_burrito_icon_vertical_position))
 
