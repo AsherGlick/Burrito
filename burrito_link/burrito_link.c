8c5830c1f96e861d180605ac5b5ffb07f05180e6
Asher Glick <asher.glick@gmail.com>
2025-05-16 17:48:42 -0500

    Embedding the context message directly instead of casting to it

--- burrito_link/burrito_link.c
+++ burrito_link/burrito_link.c
@@ -140,7 +140,7 @@ void initMumble() {
     }
     lm = (struct LinkedMem*)mapped_lm;
 
-    lc = (struct MumbleContext*)lm->context;
+    lc = &lm->context;
     printf("successfully opened mumble link shared memory..\n");
 }
 
